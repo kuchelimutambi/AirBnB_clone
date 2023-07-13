8e45e6f93458e31920f9d8b5779b53ec5d8ef748
kuchelimutambi <kuchelideogratius@gmail.com>
2023-07-13 10:05:11 +0300

    remove files on my branch

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" a unique FileStorage instance for the application, ensuring that any previously stored instances are reloaded and available for use """
-from .engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
